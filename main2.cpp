294b7654139f5b9fbdf5b9b1384eeb2238d24743
ChoiJinheon <127115703+ChoiJinheon@users.noreply.github.com>
2023-05-10 00:04:30 +0900

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -9,26 +9,22 @@ using namespace std;
 
 void main() {
 	// akili
-	character * akali;
-	akali = new character(5, 10, 2, 3, -5, 0.1, 10);
+	character * akali = new character(5, 10, 2, 3, -5, 0.1, 10);
 	akali->savefile("akali.dat");
 	delete akali;
 
 	// amumu
-	character * amumu;
-	amumu = new character(7, 100, -3.5, 4, 100, 10, 15);
+	character * amumu = new character(7, 100, -3.5, 4, 100, 10, 15);
 	amumu->savefile("amumu.dat");
 	delete amumu;
 
 	// annie
-	character * annie;
-	annie = new character(12, 17, 0.01, 0.2, 1.5, 0.08, 20);
+	character * annie = new character(12, 17, 0.01, 0.2, 1.5, 0.08, 20);
 	annie->savefile("annie.dat");
 	delete annie;
 
 	// ashe
-	character * ashe;
-	ashe = new character(10, 20, 4.2, -5.3, 2.1, 0.2, 5);
+	character * ashe = new character(10, 20, 4.2, -5.3, 2.1, 0.2, 5);
 	ashe->savefile("ashe.dat");
 	delete ashe;
 
