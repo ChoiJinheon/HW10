6b064183d451edac118e42e7992fd4d3cdd03304
ChoiJinheon <127115703+ChoiJinheon@users.noreply.github.com>
2023-05-10 00:04:03 +0900

    Update class2.h

--- class2.h
+++ class2.h
@@ -5,16 +5,7 @@ using namespace std;
 class character {
 public:
     character() {};
-    character(int N1_i, int N2_i, float a_i, float b_i, float x0_i, float dx_i, int m_i) {
-
-        N1 = N1_i;
-        N2 = N2_i;
-        a = a_i;
-        b = b_i;
-        x0 = x0_i;
-        dx = dx_i;
-        m = m_i;
-    }
+    character(int N1_i, int N2_i, float a_i, float b_i, float x0_i, float dx_i, int m_i);
     int N1, N2, m;
     float a, b, x0, dx;
     int* datafunc_1;
